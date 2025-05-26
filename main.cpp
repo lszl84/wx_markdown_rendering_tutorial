a1398a7084c062e7d438ea18c24bfde59855ba30
Luke <luke@onlyfastcode.com>
2025-05-26 19:55:13 +0200

    final code

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <wx/wx.h>
+#include <wx/html/htmlwin.h>
+#include <wx/textctrl.h>
+#include <wx/splitter.h>
+#include <cmark.h>
 
 class MyApp : public wxApp
 {
@@ -10,6 +14,11 @@ class MyFrame : public wxFrame
 {
     public:
         MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+    private:
+        void RenderMarkdown();
+        wxHtmlWindow *htmlControl;
+        wxTextCtrl *markdownControl;
+        wxTimer renderTimer;
 };
 
 wxIMPLEMENT_APP(MyApp);
@@ -68,5 +77,50 @@ bool MyApp::OnInit()
 Visit [wxWidgets Documentation](https://docs.wxwidgets.org/3.2) to learn more about wxWidgets.
     )";
 
+    wxSplitterWindow *splitter = new wxSplitterWindow(this, wxID_ANY);
+
+    htmlControl = new wxHtmlWindow(splitter, wxID_ANY);
+
+    markdownControl = new wxTextCtrl(splitter, wxID_ANY, MarkdownExample, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE );
+    markdownControl->SetValue(MarkdownExample);
+    markdownControl->SetFont(wxFontInfo(12).Family(wxFONTFAMILY_TELETYPE));
+
+    splitter->SetSashGravity(0.5);
+    splitter->SetMinimumPaneSize(FromDIP(200));
+    splitter->SplitVertically(markdownControl, htmlControl);
+
+    renderTimer.SetOwner(this);
+
+    Bind(wxEVT_TIMER, 
+            [this](wxTimerEvent& event) {
+                RenderMarkdown();
+            });
+
+    markdownControl->Bind(wxEVT_TEXT, 
+            [this](wxCommandEvent& event) {
+                if (!renderTimer.IsRunning())
+                {
+                    renderTimer.StartOnce(500); // Delay rendering for 500 ms
+                }
+            });
+    htmlControl->Bind(wxEVT_HTML_LINK_CLICKED, 
+            [this](wxHtmlLinkEvent& event) {
+                wxLaunchDefaultBrowser(event.GetLinkInfo().GetHref());
+            });
+
+
+    RenderMarkdown();
 }
 
+
+void MyFrame::RenderMarkdown()
+{
+    auto text = markdownControl->GetValue();
+    auto buffer = text.utf8_str();
+
+    auto htmlText = cmark_markdown_to_html(buffer.data(), buffer.length(), CMARK_OPT_DEFAULT);
+
+    htmlControl->SetPage(htmlText);
+
+    free(htmlText);
+}
